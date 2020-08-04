4f94eec604d40ab94030c24b0305e445be0a71fa
weaver <weavernet@163.com>
2020-08-04 11:20:29 +0800

    1.修订’价值距离‘为‘估值距离’代表了距离当前新发债券的估值距离 2.增加了上市日期和交易天数列方便查看最新上市的债券

--- WeaveNet.py
+++ WeaveNet.py
@@ -45,6 +45,14 @@ def calc_value_center_unlist(unlistpd):
 def calc_value_distance(a, b,va,vb):
 	return math.sqrt((a-va)**2+(b-vb)**2)
 
+
+
+def get_pass_days(date):
+	if date == '-':
+		return 0
+	else:
+		delt = datetime.datetime.now() - datetime.datetime.strptime(date, '%Y%m%d')
+		return delt.days
 #bond_cov_comparison_df = pd.read_excel('compare-.xls', 'compare')['最新价'].str.replace('-','')
 #bond_expect_df = bond_cov_comparison_df['转股溢价率'].map(lambda x:-x)
 #new_price_select = bond_cov_comparison_df[bond_cov_comparison_df['最新价'].astype(float) < 120.0]
@@ -55,78 +63,79 @@ def calc_value_distance(a, b,va,vb):
 
 if __name__=='__main__':
 
-	from sys import argv
-	tnow = ""
-	if len(argv) > 1:
-		if argv[1] == '*':
-			tnow = datetime.datetime.now()
-		else:
-			tnow = datetime.datetime.strptime(argv[1], '%Y/%m/%d')
-	else:
-		print("please run like 'python WeaveNet.py [*|2020/07/07]'")
-		exit(1)
+    from sys import argv
+    tnow = ""
+    if len(argv) > 1:
+        if argv[1] == '*':
+            tnow = datetime.datetime.now()
+        else:
+            tnow = datetime.datetime.strptime(argv[1], '%Y/%m/%d')
+    else:
+        print("please run like 'python WeaveNet.py [*|2020/07/07]'")
+        exit(1)
 
-	print("time is :" + tnow.strftime('%Y%m%d'))
-	
-	filefolder = r'./data/' + tnow.strftime('%Y%m%d')
-	filein = tnow.strftime('%Y_%m_%d') + '_in.xls'
-	getakpath =  "%s/%s" % (filefolder,filein) 
-	
-	isExist = os.path.exists(filefolder)
-	if not isExist:
-		os.makedirs(filefolder)
-		print("AkShareFile:%s create" % (filefolder))  
-	else:
-		print("AkShareFile:%s exist" % (filefolder))
+    print("time is :" + tnow.strftime('%Y%m%d'))
 
-	resultpath,insheetname = get_akshare_comparison(getakpath)
-	print("data of path:" + resultpath + "sheetname:" +insheetname)
-	
-	
+    filefolder = r'./data/' + tnow.strftime('%Y%m%d')
+    filein = tnow.strftime('%Y_%m_%d') + '_in.xls'
+    getakpath =  "%s/%s" % (filefolder,filein)
 
-	bond_unlisted_df = pd.read_excel(resultpath, insheetname,converters={'正股代码':str})[['转债名称','正股代码','转股溢价率','纯债溢价率','申购日期','上市日期']]
-	bond_unlisted_df = bond_unlisted_df[bond_unlisted_df['上市日期'] == '-']
-	va,vb = calc_value_center_unlist(bond_unlisted_df)
-	#va,vb = calc_value_center()
-	print("the average of unlisted bond 转股溢价率,纯债溢价率",va,vb)
+    isExist = os.path.exists(filefolder)
+    if not isExist:
+        os.makedirs(filefolder)
+        print("AkShareFile:%s create" % (filefolder))
+    else:
+        print("AkShareFile:%s exist" % (filefolder))
+
+    resultpath,insheetname = get_akshare_comparison(getakpath)
+    print("data of path:" + resultpath + "sheetname:" +insheetname)
 
 
-	bond_cov_comparison_df = pd.read_excel(resultpath, insheetname,converters={'正股代码':str})[['最新价','转债名称','正股代码','转股价值','纯债价值','转股溢价率','纯债溢价率','申购日期']]
-	bond_cov_comparison_df['价值距离'] = bond_cov_comparison_df.apply(lambda row: calc_value_distance(row['转股溢价率'], row['纯债溢价率'],va,vb), axis=1)
-	bond_expect_sort_df = bond_cov_comparison_df.sort_values('价值距离',ascending=True)
-	
-	fileout = tnow.strftime('%Y_%m_%d') + '_out.xls'
-	outanalypath =  "%s/%s" % (filefolder,fileout)
-	writer = pd.ExcelWriter(outanalypath)
-	bond_unlisted_df.to_excel(writer, 'unlist')
-	bond_expect_sort_df.to_excel(writer,'analyze')
-	writer.save()
-	print("value distance of  'unlist and analye' :" + fileout)
+
+    bond_unlisted_df = pd.read_excel(resultpath, insheetname,converters={'正股代码':str})[['转债名称','正股代码','转股溢价率','纯债溢价率','申购日期','上市日期']]
+    bond_unlisted_df = bond_unlisted_df[bond_unlisted_df['上市日期'] == '-']
+    va,vb = calc_value_center_unlist(bond_unlisted_df)
+    #va,vb = calc_value_center()
+    print("the average of unlisted bond 转股溢价率,纯债溢价率",va,vb)
 
 
-	#print(bond_expect_sort_df)
-	# 显示散点图
-	#bond_expect_sort_df.plot.scatter(x='纯债溢价率', y='转股溢价率')
-	X = bond_expect_sort_df.values
-	#plt.plot(X[:,6], X[:,5],"ro")
-	txt = X[:,1].reshape(1, -1)[0]
-	x = X[:,6].reshape(1, -1)[0]
-	y = X[:,5].reshape(1, -1)[0]
-	plt.scatter(x,y)
-	for i in range(len(txt)):
-		plt.annotate(txt[i][0:2], xy = (x[i],y[i]), xytext = (x[i]+0.1, y[i]+0.1)) #这里xy是需要标记的坐标，xytext是对应的标签坐标
-	
-		
-	# 显示图
-	plt.xlabel('纯债溢价率')
-	plt.ylabel('转股溢价率')
-	plt.rcParams['font.sans-serif']=['SimHei']
+    bond_cov_comparison_df = pd.read_excel(resultpath, insheetname,converters={'正股代码':str,'上市日期':str})[['最新价','转债名称','正股代码','转股价值','纯债价值','转股溢价率','纯债溢价率','上市日期']]
+    bond_cov_comparison_df['估值距离'] = bond_cov_comparison_df.apply(lambda row: calc_value_distance(row['转股溢价率'], row['纯债溢价率'],va,vb), axis=1)
+    bond_cov_comparison_df['交易天数'] = bond_cov_comparison_df.apply(lambda row: get_pass_days(row['上市日期']), axis=1)
+    bond_expect_sort_df = bond_cov_comparison_df.sort_values('交易天数',ascending=True)
 
-	fileimage = tnow.strftime('%Y_%m_%d') + '_image.png'
-	imagepath =  "%s/%s" % (filefolder,fileimage) 
-	plt.savefig(imagepath)
-	print("value image of  path:" + imagepath )
-	#plt.show()
+    fileout = tnow.strftime('%Y_%m_%d') + '_out.xls'
+    outanalypath =  "%s/%s" % (filefolder,fileout)
+    writer = pd.ExcelWriter(outanalypath)
+    bond_unlisted_df.to_excel(writer, 'unlist')
+    bond_expect_sort_df.to_excel(writer,'analyze')
+    writer.save()
+    print("value distance of  'unlist and analye' :" + fileout)
+
+
+    #print(bond_expect_sort_df)
+    # 显示散点图
+    #bond_expect_sort_df.plot.scatter(x='纯债溢价率', y='转股溢价率')
+    X = bond_expect_sort_df.values
+    #plt.plot(X[:,6], X[:,5],"ro")
+    txt = X[:,1].reshape(1, -1)[0]
+    x = X[:,6].reshape(1, -1)[0]
+    y = X[:,5].reshape(1, -1)[0]
+    plt.scatter(x,y)
+    for i in range(len(txt)):
+        plt.annotate(txt[i][0:2], xy = (x[i],y[i]), xytext = (x[i]+0.1, y[i]+0.1)) #这里xy是需要标记的坐标，xytext是对应的标签坐标
+
+
+    # 显示图
+    plt.xlabel('纯债溢价率')
+    plt.ylabel('转股溢价率')
+    plt.rcParams['font.sans-serif']=['SimHei']
+
+    fileimage = tnow.strftime('%Y_%m_%d') + '_image.png'
+    imagepath =  "%s/%s" % (filefolder,fileimage)
+    plt.savefig(imagepath)
+    print("value image of  path:" + imagepath )
+    #plt.show()
 
 
 
